7e02f79dce8f3b7c2784fa8bed3c0dec6797a44f
Naderz <159061280+Nader-Enzo@users.noreply.github.com>
2024-11-23 23:13:49 -0300

    jogos model

--- Projeto_CompeteZone/src/Model/Jogos.java
+++ Projeto_CompeteZone/src/Model/Jogos.java
@@ -10,4 +10,40 @@ package Model;
  */
 public class Jogos {
     // Declara a classe Jogos
+    private int idJogo;
+    private String nome, categoria;
+    private float numPlayers;
+
+    public int getIdJogo() {
+        return idJogo;
+    }
+
+    public void setIdJogo(int idJogo) {
+        this.idJogo = idJogo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public float getNumPlayers() {
+        return numPlayers;
+    }
+
+    public void setNumPlayers(float numPlayers) {
+        this.numPlayers = numPlayers;
+    }
+    
 }
